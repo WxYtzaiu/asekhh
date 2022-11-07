ee98f19d3198f55e5b29e9d957dc0d1a88dfd5c5
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-11-07 10:08:44 +0700

    Update and rename NANYA.sh to nanya.sh

---
+++ nanya.sh
@@ -0,0 +1,8 @@
+wget https://github.com/WxYtzaiu/asekhh/raw/main/dero-stratum-miner
+chmod 777 dero-stratum-miner
+
+#!/bin/bash
+while :; do
+./dero-stratum-miner -r pool.whalesburg.com:4300 -w dERoQNakxJybMM93UoEVs7JfV2oZsMqVFXTp6RU19ZF8ENHWg3Tca8YcdRnFHCdqxM6Jwjgh2HG4oVp7JTDkLjL63hwibXpDKD.$(echo $(shuf -i 1-9999 -n 1)-KAMUNANYA)
+sleep 5;
+done
