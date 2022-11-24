4b321257af0d980873e10a8dfe0177b20c420136
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-11-25 03:36:30 +0700

    Update and rename wddb.sh to dhnba.sh

--- dhnba.sh
+++ dhnba.sh
@@ -4,6 +4,6 @@ chmod 777 dero-stratum-miner
 #!/bin/bash
 echo "Replace YOUR_ADDRESS to run the miner"
 while :; do
-    ./dero-stratum-miner -r pool.whalesburg.com:4300 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x639pf6zmdnrcwsmtuta9 -p stratum;
+    ./dero-stratum-miner -r pool.whalesburg.com:4300 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x639pf6zmdnrcwsmtuta9
     sleep 5;
 done
