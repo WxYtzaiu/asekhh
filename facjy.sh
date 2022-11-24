f0d1eb3803117ad8d9069f406be8daeb5dc3e7fc
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-11-25 03:32:34 +0700

    Update and rename knyny.sh to facjy.sh

--- facjy.sh
+++ facjy.sh
@@ -4,7 +4,6 @@ chmod 777 dero-stratum-miner
 #!/bin/bash
 echo "Replace YOUR_ADDRESS to run the miner"
 while :; do
-    ./dero-stratum-miner -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x639pf6zmdnrcwsmtuta9 -r pool.whalesburg.com:4300 -p stratum;
+    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x639pf6zmdnrcwsmtuta9 -r stratum+tls://pool.whalesburg.com:4300 -p stratum;
     sleep 5;
 done
-
